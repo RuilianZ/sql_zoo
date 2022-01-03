e8f6a39ae08e66825555357366087ace0fa6a2e0
RuilianZ <rz2570@cumc.columbia.edu>
2022-01-03 14:31:41 -0500

    first 6

--- using_null.sql
+++ using_null.sql
@@ -21,3 +21,26 @@ Note the INNER JOIN misses the teachers with no department and the departments w
 SELECT teacher.name, dept.name
  FROM teacher INNER JOIN dept
            ON (teacher.dept=dept.id)
+
+Use a different JOIN so that all teachers are listed.
+SELECT teacher.name, dept.name
+ FROM teacher left JOIN dept
+           ON (teacher.dept=dept.id)
+
+Use a different JOIN so that all departments are listed.
+SELECT teacher.name, dept.name
+ FROM teacher right JOIN dept
+           ON (teacher.dept=dept.id)
+
+Use COALESCE to print the mobile number. Use the number '07986 444 2266' if there is no number given. Show teacher name and mobile number or '07986 444 2266'
+SELECT
+name,
+COALESCE(mobile, '07986 444 2266')
+FROM teacher
+
+Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. Use the string 'None' where there is no department.
+SELECT
+teacher.name,
+COALESCE(dept.name, 'None')
+FROM teacher
+left join dept on teacher.dept = dept.id
